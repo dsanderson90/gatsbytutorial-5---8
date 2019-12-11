bc19edf36c426871ba3d51c3d4cd3949579e15a6
dsanderson90 <dsanderson3@cnm.edu>
2019-12-11 11:41:02 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+export default ( { data }) => (
+  <Layout>
+    <h1>{data.site.siteMetaData.title} Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
+)
 
-export default () => <div>Hello world!</div>
+export const query = graphql`
+query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+}
+`
