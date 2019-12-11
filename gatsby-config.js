c377ea1a05ae80ffca1ea5cd12d78e75a6503673
dsanderson90 <dsanderson3@cnm.edu>
2019-12-11 13:42:31 -0700

    completed section 8

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,24 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eatings lots...`,
+    author:  `David`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,8 +31,10 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-emotion`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
